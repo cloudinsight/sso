dd19d9fb7f9765b7b2770c5bce31dc1f2b55679f
wangchen <wangchen@oneapm.com>
2016-08-11 17:17:38 +0800

    fix

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,6 @@
 import styles from './templates/footer.css';
 import grids from 'purecss/build/grids.css';
+import $ from 'jquery';
 import responsiveGrids from 'purecss/build/grids-responsive.css';
 
 const style = Object.assign({}, styles, grids, responsiveGrids);
@@ -10,6 +11,17 @@ var element = document.createElement('div');
 element.className = style.footer;
 element.innerHTML = compiled({
   style
+});
+
+var nowDate = new Date();
+$(document).ready(function () {
+  $('.sso-cr-end-time').text(nowDate.getFullYear());
+  $('.sso-wechat').mouseover(function () {
+    $('.sso-wechatQR').addClass('sso-show');
+  })
+  $('.sso-wechat').mouseout(function () {
+    $('.sso-wechatQR').removeClass('sso-show');
+  })
 });
 
 document.body.appendChild(element);
--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,5 @@
 import styles from './templates/nav.css';
-
+import $ from 'jquery';
 const style = Object.assign({}, styles);
 
 import compiled from './templates/nav.ejs';
@@ -8,6 +8,19 @@ var element = document.createElement('div');
 element.className = style.nav;
 element.innerHTML = compiled({
   style
+});
+$(document).ready(function(){
+  $('.sso-burger-wrapper').click(function(){
+    if($('.sso-main-navblock').hasClass('sso-navOpen')){
+      $('body').removeClass('sso-homeShow');
+      $('.sso-main-navblock').removeClass('sso-navOpen');
+      $('.sso-burger').removeClass('sso-active-burger');
+    }else{
+      $('body').addClass('sso-homeShow');
+      $('.sso-main-navblock').addClass('sso-navOpen');
+      $('.sso-burger').addClass('sso-active-burger');
+    }
+  });
 });
 
 document.body.appendChild(element);
