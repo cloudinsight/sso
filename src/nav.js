a649ad5e63f3b4b9d67739bb3c379145a8154950
wangchen <wangchen@oneapm.com>
2016-11-09 11:28:02 +0800

    Complete the landing

--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,8 @@
 import styles from './templates/nav.css';
 import $ from 'jquery';
+import sweetAlert from 'sweetalert';
+import '!style!css!sweetalert/dist/sweetalert.css';
+
 const style = Object.assign({}, styles);
 
 import compiled from './templates/nav.ejs';
@@ -17,17 +20,20 @@ element.className = style.nav;
 element.innerHTML = compiled({
   style
 });
-var demo = [
-  {
-    groupName: '蓝海讯通有限公司',
-    used: true
-  },{
-    groupName: 'blueware',
-    used: false
-  },{
-    groupName:'OneApm',
-    used: false
-  }];
+// var demo = [
+//   {
+//     groupName: '蓝海讯通有限公司',
+//     defaultGroup: true,
+//     groupId: 123
+//   },{
+//     groupName: 'blueware',
+//     defaultGroup: false,
+//     groupId: 456
+//   },{
+//     groupName:'OneApm',
+//     defaultGroup: false,
+//     groupId: 789
+//   }];
 function setInfo(data) {
   for (var i=0; i < data.length;i++) {
     var infoItem = document.createElement('li');
@@ -40,6 +46,7 @@ function setInfo(data) {
     $('.'+style['right']).parent().parent().css('background-color','#fff');
     infoBox.className = style['clearfix'];
     Span.innerHTML = data[i].groupName;
+    $(Span).attr('key', data[i].groupId);
     infoBox.appendChild(Span);
     infoBox.appendChild(rightIcon);
     infoItem.appendChild(infoBox);
@@ -61,15 +68,40 @@ function getInfo() {
     }
   })
 }
+function setCookie(name,value,days){
+  var exp=new Date();
+  exp.setTime(exp.getTime() + days*24*60*60*1000);
+  var arr=document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
+  document.cookie=name+"="+escape(value)+";expires="+exp.toGMTString()+";path=/";
+}
+function getCookie(name){
+  var arr=document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
+  if(arr!=null){
+    return unescape(arr[2]);
+    return null;
+  }
+}
+function delCookie(name){
+  var exp=new Date();
+  exp.setTime(exp.getTime()-1);
+  var cval=getCookie(name);
+
+  if(cval!=null){
+    document.cookie=name+"="+cval+";expires="+exp.toGMTString();
+  }
+}
 $(document).ready(function(){
   getInfo();
+  // setInfo(demo);
+  setCookie('key','123');
   // 绑定进入系统接口
   $('.'+style['infoList']).children().each(function() {
     if($(this).find('span').text()){
-      $(this).click(function (e) {
+      $(this).click(function () {
+        var group_id= $(this).find('span').attr('key');
         $.ajax({
           type: 'GET',
-          url: `${http}//${host}${BlueWare.urlPrefix}user/groups/${e.target.key}/switch`
+          url: `${http}//${host}${BlueWare.urlPrefix}user/groups/${group_id}/switch`
         })
       });
     }
@@ -84,6 +116,27 @@ $(document).ready(function(){
       $('.'+style['burger']).addClass(style['active-burger']);
     }
   });
+  $('.'+style['changeAccount']).click(function () {
+    swal({
+          title: "确定继续 ?",
+          text: "此操作将退出当前账号",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          closeOnConfirm: false,
+          closeOnCancel: false
+        },
+        function(isConfirm){
+          if (isConfirm) {
+            delCookie('key');
+            swal.close();
+          } else {
+            swal.close();
+          }
+        })
+  })
 });
 
 document.body.appendChild(element);
